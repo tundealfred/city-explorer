515c151af3a658560e232c9a3ce1140abcc3f158
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-10-30 15:48:19 +0000

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    const res = await axios.get(API);
+
+    setLocation(res.data[0]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>City Explorer!!!</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} required placeholder="Location" />
+        <button>Explore!</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+      <h3>{location.lat}</h3>
+      <h3>{location.lon}</h3>
+
+      {/* //Display name
+    //Latitude
+    //Longitude */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
